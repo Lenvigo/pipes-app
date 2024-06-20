f32588cdb12681e4ae2af0b9af36b90910e1228b
Elena Gonzalez <elena.gonzalezlago@plexus.es>
2024-06-20 11:23:38 +0200

    add menu

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+
+})
+export class MenuComponent implements OnInit {
+
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          { label: 'Textos y fechas', icon: 'pi pi-align-left', },
+          { label: 'Números', icon: 'pi pi-dollar', },
+          { label: 'No comunes', icon: 'pi pi-globe', }
+        ]
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          { label: 'Otro elemento', icon: 'pi pi-cog', }
+        ]
+      }
+    ];
+  }
+
+}
